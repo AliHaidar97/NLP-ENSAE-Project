95aebb5ac441d3cec0649ceb4b14d555a2e8be0f
ali haidar <alihaidar.97@hotmail.com>
2023-03-16 17:44:30 +0100

    update the code

--- models.py
+++ models.py
@@ -15,100 +15,131 @@ from IPython.display import clear_output
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.layers import Input, Dense, Embedding, SpatialDropout1D, add, concatenate,Flatten
-from keras.layers import CuDNNLSTM, Bidirectional, GlobalMaxPooling1D, GlobalAveragePooling1D, LSTM
+from keras.layers import CuDNNLSTM, LSTM, Bidirectional, GlobalMaxPooling1D, GlobalAveragePooling1D, LSTM, Lambda,MaxPooling1D, AveragePooling1D,GRU, Reshape
 from keras.preprocessing import text, sequence
 from gensim.models import KeyedVectors
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.optimizers.schedules import PolynomialDecay
 
 
+
 class BertMLP1Layer:
-
-  def __init__(self,  dropout = 0.2):
-
-    self.dropout = 0.2
-
   
-  def build_model(self, embedding_matrix, num_aux_targets):
-      words = Input(shape=(512))
-      x = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False, mask_zero = True)(words)
-      x = SpatialDropout1D(self.dropout)(x)
-      x = Flatten()(x)
-      aux_result = Dense(num_aux_targets, activation='softmax')(x)
-      model = Model(inputs=words, outputs=aux_result)
-      return model
+    def __init__(self, dropout=0.2):
+        # Constructor method that sets the value of the dropout rate.
+        self.dropout = dropout
+  
+    def build_model(self, embedding_matrix, labels, out=1):
+        # Method to build and return the Keras model.
+        # Input:
+        #   embedding_matrix: Pre-trained word embeddings matrix.
+        #   labels: Number of labels.
+        #   out: Output shape of the model.
+        # Output:
+        #   Keras model.
+
+        # Input layer.
+        words = Input(shape=(512,))
+
+        # Embedding layer.
+        x = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False, mask_zero=True)(words)
+
+        # Dropout layer.
+        x = SpatialDropout1D(self.dropout)(x)
+
+        # Concatenation layer of GlobalMaxPooling1D and GlobalAveragePooling1D.
+        x = concatenate([
+            GlobalMaxPooling1D()(x),
+            GlobalAveragePooling1D()(x)
+        ])
+
+        # Dense layer with softmax activation.
+        logits = Dense(labels*out, activation='softmax')(x)
+
+        # Reshape layer for multi-targets.
+        if out != 1:
+            logits = Reshape((out, labels))(logits)
+    
+        # Model creation.
+        model = Model(inputs=words, outputs=logits)
+      
+        return model
+
 
 
 class BertMLP2Layers:
 
   def __init__(self, HIDDEN = 128, dropout = 0.2):
-
+    # Initialize the class with the given hyperparameters.
     self.HIDDEN = HIDDEN
     self.dropout = 0.2
 
   
-  def build_model(self, embedding_matrix, num_aux_targets):
-      words = Input(shape=(512,))
-      x = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False)(words)
-      x = SpatialDropout1D(self.dropout)(x)
-      x = Flatten()(x)
-      x = Dense(self.HIDDEN, activation='relu')(x)
-      aux_result = Dense(num_aux_targets, activation='softmax')(x)
-      
-      model = Model(inputs=words, outputs=aux_result)
-   
-      return model
+  def build_model(self, embedding_matrix, labels, out=1):
+    # Define the architecture of the model.
+    # Input layer.
+    words = Input(shape=(512,))
+    # Embedding layer with pre-trained embeddings.
+    x = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False, mask_zero=True)(words)
+    # Dropout layer.
+    x = SpatialDropout1D(self.dropout)(x)
+    # Concatenation of GlobalMaxPooling1D and GlobalAveragePooling1D layers.
+    x = concatenate([
+        GlobalMaxPooling1D()(x),
+        GlobalAveragePooling1D()(x),
+    ])
+    # First dense layer with ReLU activation.
+    x = Dense(self.HIDDEN, activation='relu')(x)
+    # Output layer with softmax activation.
+    logits = Dense(labels*out, activation='softmax')(x)
+    # Reshape for mutlti targets.
+    if(out != 1):
+        logits = Reshape((out, labels))(logits)
+    # Define and return the model.
+    model = Model(inputs=words, outputs=logits)
+    return model
+
+
     
-class BertLstm:
-
-  def __init__(self,  LSTM_UNITS = 64, dropout = 0.2):
- 
-    self.LSTM_UNITS = LSTM_UNITS
-    self.dropout = 0.2
-    self.DENSE_HIDDEN_UNITS = 4 * LSTM_UNITS
-
-        
+class BertGRU:
   
-  def build_model(self, embedding_matrix, num_aux_targets):
-      words = Input(shape=(None,))
-      x = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False)(words)
-      x = SpatialDropout1D(self.dropout)(x)
-      x = Bidirectional(CuDNNLSTM(self.LSTM_UNITS , return_sequences=True))(x)
-      x = Bidirectional(CuDNNLSTM(self.LSTM_UNITS , return_sequences=True))(x)
-      hidden = concatenate([
-          GlobalMaxPooling1D()(x),
-          GlobalAveragePooling1D()(x),
-      ])
-      hidden = add([hidden, Dense(self.DENSE_HIDDEN_UNITS, activation='relu')(hidden)])
-      aux_result = Dense(num_aux_targets, activation='softmax')(hidden)
-      
-      model = Model(inputs=words, outputs=aux_result)
-    
-      return model
-  
-  
-class BertDoubleLstm:
+  def __init__(self, GRU_UNITS=128, dropout=0.2):
+    self.GRU_UNITS = GRU_UNITS
+    self.dropout = dropout
+    self.DENSE_HIDDEN_UNITS = 4 * GRU_UNITS  # number of units in the dense hidden layer
 
-  def __init__(self, LSTM_UNITS = 64, dropout = 0.2):
-      
-      self.LSTM_UNITS = LSTM_UNITS
-      self.dropout = 0.2
-      self.DENSE_HIDDEN_UNITS = 4 * LSTM_UNITS
+  def build_model(self, embedding_matrix, labels, out=1):
+    words = Input(shape=(None,))
+    x = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False, mask_zero=True)(words)
 
-  def build_model(self,embedding_matrix, num_aux_targets):
-      words = Input(shape=(None,))
-      x = Embedding(*embedding_matrix.shape, weights=[embedding_matrix], trainable=False)(words)
-      x = SpatialDropout1D(self.dropout)(x)
-      x = Bidirectional(CuDNNLSTM(self.LSTM_UNITS, return_sequences=True))(x)
-      x = Bidirectional(CuDNNLSTM(self.LSTM_UNITS, return_sequences=True))(x)
-      hidden = concatenate([
-          GlobalMaxPooling1D()(x),
-          GlobalAveragePooling1D()(x),
-      ])
-      hidden = add([hidden, Dense(self.DENSE_HIDDEN_UNITS, activation='relu')(hidden)])
-      hidden = add([hidden, Dense(self.DENSE_HIDDEN_UNITS, activation='relu')(hidden)])
-      aux_result = Dense(num_aux_targets, activation='softmax')(hidden)
-      
-      model = Model(inputs=words, outputs=aux_result)
- 
-      return model
+    x = SpatialDropout1D(self.dropout)(x)
+    # Bidirectional GRU layer, returns sequences
+    y = Bidirectional(GRU(self.GRU_UNITS, return_sequences=True))(x)
+    y = SpatialDropout1D(self.dropout)(y)  # apply spatial dropout
+    # Bidirectional GRU layer, returns sequences
+    z = Bidirectional(GRU(self.GRU_UNITS, return_sequences=True))(y)
+
+    # Global pooling layers for each GRU layer output
+    hidden_x = concatenate([GlobalMaxPooling1D()(x), GlobalAveragePooling1D()(x)], axis=1)
+    hidden_y = concatenate([GlobalMaxPooling1D()(y), GlobalAveragePooling1D()(y)], axis=1)
+    hidden_z = concatenate([GlobalMaxPooling1D()(z), GlobalAveragePooling1D()(z)], axis=1)
+
+    # Concatenate the global pooling outputs from each GRU layer
+    hidden = concatenate([hidden_x, hidden_y, hidden_z], axis=1)
+    hidden = Dense(self.DENSE_HIDDEN_UNITS, activation='relu')(hidden)  # dense hidden layer
+    logits = Dense(labels * out, activation='softmax')(hidden)  # output layer with softmax activation
+
+    if out != 1:
+      logits = Reshape((out, labels))(logits)  # Reshape for mutlti targets.
+
+    # create the model
+    model = Model(inputs=words, outputs=logits)
+    return model
+
+
+def get_bert_embed_matrix():
+    bert = BertModel.from_pretrained('bert-base-uncased')
+    bert_embeddings = list(bert.children())[0]
+    bert_word_embeddings = list(bert_embeddings.children())[0]
+    mat = bert_word_embeddings.weight.data.numpy()
+    return mat
--- process.py
+++ process.py
@@ -12,55 +12,68 @@ from torch.nn.utils import clip_grad_norm_
 from IPython.display import clear_output
 
 
-def tokenize(text, tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)):
-    
-    tokens = list(map(lambda t: ["[CLS]"] + tokenizer.tokenize(t)[:510] + ["[SEP]"] , text)) #The max size of bert input is 512
+def tokenize(text, tokenizer):
+    # Add [CLS] and [SEP] tokens to each text and truncate to 510 tokens
+    tokens = list(map(lambda t: ["[CLS]"] + tokenizer.tokenize(t)[:510] + ["[SEP]"], text))
+    # Convert tokens to token ids
     tokens_ids = list(map(tokenizer.convert_tokens_to_ids, tokens))
-    tokens_ids = pad_sequences(tokens_ids, maxlen=512, truncating="post", padding="post", dtype="int",value=-1) #Pad to get 512
-    
+    # Pad the token ids to a length of 512
+    tokens_ids = pad_sequences(tokens_ids, maxlen=512, truncating="post", padding="post", dtype="int")
     return tokens_ids
+
+
 
 def mask(token_ids):
     
-    return [[int(i >= 0) for i in ii] for ii in token_ids]
+    return [[i > 0 for i in ii] for ii in token_ids]
 
 
 
-
-
-def context(train, test, sizeOfTheContext = -1):
+def context(train, val, test, sizeOfTheContext=-1):
     
-    train_modified = train[['Dialogue_ID','Utterance','Label']]
-    train_modified['Label'] = train_modified['Label'].apply(lambda x:[x])
+    train_modified = train[['Dialogue_ID', 'Utterance', 'Label']].copy()
+    train_modified['Label'] = train_modified['Label'].apply(lambda x: [x])
     train_modified = train_modified.groupby('Dialogue_ID').agg(sum).reset_index()
+    
+    val_modified = val[['Dialogue_ID', 'Utterance', 'Label']].copy()
+    val_modified['Label'] = val_modified['Label'].apply(lambda x: [x])
+    val_modified = val_modified.groupby('Dialogue_ID').agg(sum).reset_index()
 
-    test_modified = test[['Dialogue_ID','Utterance','Label']]
-    test_modified['Label'] = test_modified['Label'].apply(lambda x:[x])
+    test_modified = test[['Dialogue_ID', 'Utterance', 'Label']].copy()
+    test_modified['Label'] = test_modified['Label'].apply(lambda x: [x])
     test_modified = test_modified.groupby('Dialogue_ID').agg(sum).reset_index()
 
-    if(sizeOfTheContext != -1):
-        #Filter the context
-        train_modified['len'] = train_modified.apply(lambda x: len(x['Label']),axis=1)
-        test_modified['len'] = test_modified.apply(lambda x: len(x['Label']),axis=1)
+    if sizeOfTheContext != -1:
+        # Filter the context
+        train_modified['len'] = train_modified['Label'].apply(len)
+        test_modified['len'] = test_modified['Label'].apply(len)
+        val_modified['len'] = val_modified['Label'].apply(len)
         
-        train_modified['filter']  = (train_modified['len'] < sizeOfTheContext)*1
-        test_modified['filter']  = (test_modified['len'] < sizeOfTheContext)*1
-        train_modified = train_modified[(train_modified['filter'] == 1)]
-        test_modified = test_modified[(test_modified['filter'] == 1)]
+        train_modified = train_modified[train_modified['len'] < sizeOfTheContext]
+        test_modified = test_modified[test_modified['len'] < sizeOfTheContext]
+        val_modified = val_modified[val_modified['len'] < sizeOfTheContext]
         
-        train_modified = train_modified.drop(['len','filter'],axis=1)
-        test_modified = test_modified.drop(['len','filter'],axis=1)
+        train_modified = train_modified.drop(['len'], axis=1)
+        test_modified = test_modified.drop(['len'], axis=1)
+        val_modified = val_modified.drop(['len'], axis=1)
 
-    if(sizeOfTheContext == -1):
-        sizeOfTheContext = max(np.max(train_modified.apply(lambda x: len(x['Label']),axis=1)), np.max(test_modified.apply(lambda x: len(x['Label']),axis=1))) 
+    if sizeOfTheContext == -1:
+        sizeOfTheContext = max(
+            np.max(val_modified['Label'].apply(len)),
+            np.max(train_modified['Label'].apply(len)),
+            np.max(test_modified['Label'].apply(len))
+        )
 
-        
-    pad = list(pad_sequences(train_modified['Label'], maxlen=sizeOfTheContext, truncating="post", padding="post", dtype="int", value = -1))
-    pad = [list(i) for i in pad]
+    pad = list(pad_sequences(train_modified['Label'], maxlen=sizeOfTheContext, truncating='post', padding='post', dtype='int', value=-1))
+    pad = [[j for j in i] for i in pad]
     train_modified['Label'] = pad
 
-    pad = list(pad_sequences(test_modified['Label'], maxlen=sizeOfTheContext, truncating="post", padding="post", dtype="int", value = -1))
-    pad = [list(i) for i in pad]
+    pad = list(pad_sequences(test_modified['Label'], maxlen=sizeOfTheContext, truncating='post', padding='post', dtype='int', value=-1))
+    pad = [[j for j in i] for i in pad]
     test_modified['Label'] = pad
     
-    return train_modified, test_modified
+    pad = list(pad_sequences(val_modified['Label'], maxlen=sizeOfTheContext, truncating='post', padding='post', dtype='int', value=-1))
+    pad = [[j for j in i] for i in pad]
+    val_modified['Label'] = pad
+    
+    return train_modified, val_modified, test_modified
